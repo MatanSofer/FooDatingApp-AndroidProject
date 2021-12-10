58b7ef1f1e561af5a82ad1697185c4cad8385a2a
Matan Ohayon <matan15595m@gmail.com>
2021-12-10 16:59:09 +0200

    Updating fragment + added buttons
    Added :Fragments,buttons,menu bar and some icons

--- app/src/main/java/com/example/finalproject_foodating/MainActivity.java
+++ app/src/main/java/com/example/finalproject_foodating/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.finalproject_foodating;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_activity_menu,menu);
+         return true;
     }
 }
