f9f3583ca437553e303493d8c4cba5f0a2255d4a
MatanSofer <73594002+MatanSofer@users.noreply.github.com>
2021-12-11 17:56:11 +0200

    nav edit

--- app/src/main/java/com/example/finalproject_foodating/MainActivity.java
+++ app/src/main/java/com/example/finalproject_foodating/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalproject_foodating;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -8,18 +9,29 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.MainActivity_fragmentContainerView);
-        NavController navController = navHostFragment.getNavController();
+        navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this,navController);
 
     }
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                navController.navigateUp();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/finalproject_foodating/MessagesFragment.java
+++ app/src/main/java/com/example/finalproject_foodating/MessagesFragment.java
@@ -17,4 +17,5 @@ public class MessagesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
     }
+
 }
